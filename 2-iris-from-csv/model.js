0a9da061098fa35a31e87b0d7ef6c7faec55cdd8
Nitin Pasumarthy <nipasuma@linkedin.com>
2019-12-17 22:20:59 +0530

    linear regression and iris

---
+++ 2-iris-from-csv/model.js
@@ -0,0 +1,121 @@
+/**
+ * Tensorflow JS code which trains and tests a model
+ * Assumes tfjs is imported
+ */
+const DATASET_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data";
+
+function createModel() {
+    const model = tf.sequential();
+    model.add(tf.layers.dense({
+        inputShape: [4],
+        units: 5,
+        activation: "relu"
+    }));
+    model.add(tf.layers.dense({
+        activation: "softmax",
+        units: 3
+    }));
+
+    model.compile({
+        loss: "categoricalCrossentropy",
+        optimizer: tf.train.adam(0.003),
+        metrics: ['accuracy']
+    })
+    return model;
+}
+
+function createData(irisUrl, batchSize = 10) {
+    const oneHotEncoderY = {
+        "Iris-setosa": [1, 0, 0],
+        "Iris-versicolor": [0, 1, 0],
+        "Iris-virginica": [0, 0, 1]
+    }
+    const csvDataset = tf.data.csv(
+        irisUrl, {
+            hasHeader: false,
+            columnNames: ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"],
+            columnConfigs: {
+                species: {
+                    isLabel: true
+                }
+            }
+        });
+    // csvDataset is of the form:
+    // xs: {sepal_length: 5.1, sepal_width: 3.5, petal_length: 1.4, petal_width: 0.2}
+    // ys: {species: "Iris-setosa"}
+    const data = csvDataset.map(({
+        xs,
+        ys
+    }) => {
+        // Convert xs(features) and ys(labels) from object form (keyed by
+        // column name) to array form.
+        return {
+            xs: Object.values(xs),
+            ys: Object.values(ys)
+        };
+    })
+        .map(({
+            xs,
+            ys
+        }) => {
+            // One hot encode y column
+            return {
+                xs,
+                ys: oneHotEncoderY[ys]
+            }
+        })
+        .batch(batchSize);
+    return data;
+}
+
+async function train(model, data, epochs = 100) {
+    const history = await model.fitDataset(data, {
+        epochs: epochs,
+        callbacks: {
+            onEpochEnd: async (epoch, logs) => {
+                if (epoch % 10 == 0) {
+                    console.debug(`Metrics: ${JSON.stringify(logs)} after ${epoch} epochs`);
+                }
+            }
+        }
+    });
+    return history;
+}
+
+async function main() {
+    const model = createModel();
+    const data = await createData("iris_data.csv");
+    // data.forEachAsync(d => d.print());
+    const history = await train(model, data);
+    // console.debug(history);
+    console.info("Done training!")
+    return model;
+}
+
+async function modelPredict(model, input = [5.8, 2.7, 5.1, 1.9]) {
+    const flowerNameFromIndex = {
+        0: "setosa",
+        1: "versicolor",
+        2: "virginica"
+    }
+
+    console.debug(`Predicting, ${input}`);
+    const probabilities = model.predict(tf.tensor2d(input, [1, 4]))
+    console.debug(`Probabilities:`);
+    probabilities.print();
+    const predictions = await probabilities.argMax(-1).array();
+    const confidenceScores = await probabilities.max(-1).array();
+    console.debug(
+        `This is ${flowerNameFromIndex[predictions[0]]} with ${(confidenceScores[0] * 100).toFixed(2)}% confidence`
+    );
+    return { "prediction": flowerNameFromIndex[predictions[0]], "confidence": confidenceScores[0]};
+}
+
+async function callMain() {
+    const model = await main();
+    await modelPredict(model);
+    return model;
+
+    // return tf.tidy(async () => {
+    // })
+}
---
+++ 2-iris-from-csv/view-model.js
@@ -0,0 +1,73 @@
+/**
+ * The script that connection functions between the UI and business logic
+ */
+
+let state = {
+    model: null,
+    alertObj: null
+}
+
+function init() {
+    state.alertObj = document.getElementById("alert_div")
+}
+
+/**
+ * helper method to sets an alert message with the required priority level
+ * @param {string} msg what message to show in the alert box
+ * @param {string} type the bootstrap css class from https://getbootstrap.com/docs/4.4/components/alerts/#examples
+ */
+function setAlertMsg(msg = "", type = "info") {
+    const validTypes = ["primary", "secondary", "success", "danger", "warning", "info", "light", "dark"];
+    const isValidType = validTypes.indexOf(type) >= 0;
+    const newCssClass = isValidType ? `alert-${type}` : "alert-info";
+    const {
+        alertObj
+    } = state
+
+    if (alertObj) {
+        if (isValidType) {
+            // Remove every other "alert-" CSS class
+            Array.from(alertObj.classList).filter(c => !c.startsWith("alert-")).forEach(c => alertObj.classList.remove(c));
+            alertObj.classList.add(newCssClass);
+        }
+        alertObj.innerHTML = msg;
+        alertObj.scrollIntoView();
+    } else {
+        alert(msg);
+    }
+}
+
+async function onStartTraininbBtnClick() {
+    setAlertMsg(`<strong>Training started!</strong> Check the status in browser console.`)
+    state.model = await callMain(); // available from model.js which is imported before this file
+    setAlertMsg("A model is trained!", "success");
+}
+
+async function onPredictBtnClick() {
+    const sepalLength = parseFloat(document.getElementById("sepal_length").value);
+    const sepalWidth = parseFloat(document.getElementById("sepal_width").value);
+    const petalLength = parseFloat(document.getElementById("petal_length").value);
+    const petalWidth = parseFloat(document.getElementById("petal_width").value);
+
+    const predictionBox = document.getElementById("prediction_box");
+    const isTrainedModelAvailable = !!state.model
+
+    try {
+        if (!isTrainedModelAvailable) {
+            setAlertMsg("Please train a model first to make predictions.");
+            return;
+        }
+        const { prediction, confidence } = await modelPredict(state.model, [sepalLength, sepalWidth, petalLength, petalWidth]);
+        document.getElementById("prediction_confidence_container").innerHTML = `${(confidence * 100).toFixed(2)}%`
+        document.getElementById("prediction_container").innerHTML = prediction;
+
+        predictionBox.classList.remove("d-none");
+        predictionBox.scrollIntoView();
+    } catch (error) {
+        setAlertMsg("<strong>Something wrong happened!</strong> Please check browser console for details.", "danger");
+        console.error(error);
+    }
+    
+}
+
+init();
